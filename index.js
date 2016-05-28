a4d3b3573c46b16353d4f2b13cb3cd9a8e551f11
Grant Gillam <gegillam@gmail.com>
2016-05-27 22:24:39 -0400

    slightly simplify usage (get rid of .developers on requires)

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var developersExcuses = require('./excuses/developers.json').excuses;
 
 module.exports = {
-    developers: {
-        getAll: function () {
-            return developersExcuses;
-        },
+      getAll: function () {
+          return developersExcuses;
+      },
 
-        getRandom: function () {
-            return developersExcuses[Math.floor(Math.random() * developersExcuses.length)];
-        }
-    }
+      getRandom: function () {
+          return developersExcuses[Math.floor(Math.random() * developersExcuses.length)];
+      }
 };
